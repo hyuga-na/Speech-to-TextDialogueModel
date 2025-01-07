a3f0b3a13da0aa1fdf42b23dcaf59d04cd1a8e37
hyuga-na <130551048+hyuga-na@users.noreply.github.com>
2025-01-07 10:32:30 +0900

    Update pretraining.py

--- Model/pretraining.py
+++ Model/pretraining.py
@@ -4,13 +4,21 @@ import sys
 import torch
 import transformers
 from transformers import  AutoTokenizer, AutoModelForCausalLM
-from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training, TaskType, PeftModel
+from peft import prepare_model_for_kbit_training
 from typing import Dict
 import pdb
 import torch
 import argparse
 import organize_data
 from transformers import TrainerCallback, TrainingArguments, TrainerState, TrainerControl
+
+
+parser = argparse.ArgumentParser(description="text dialogue model predict response and input's emotion label")
+
+parser.add_argument("-d", "--debug", action="store_true", help="this is flag to use small debug data")
+
+
+args = parser.parse_args()
 
 
 """学習進歩を見るために学習中に推論を出力するためのCallback
@@ -109,38 +117,7 @@ def get_wav_ids(features):
 
 モデルの全てのパラメータを学習し, save_model_pathに保存する
 """
-def pretraining_text(save_path, save_model_path, pretrained_path, save_log_path, train_path, val_path, test_path):
-    model = AutoModelForCausalLM.from_pretrained(
-        pretrained_path,
-        device_map="sequential",
-        )
-    tokenizer = AutoTokenizer.from_pretrained(pretrained_path, use_fast=False)
-
-    print(model)
-    print(model.print_trainable_parameters())
-
-
-    """build dataset
-    List[ Dict{ 
-        "input_text": 入力テキスト,
-        "output_text": 応答テキスト,
-        "emotion": 感情
-        "input_ids": tokenize("<s>ユーザー: {入力テキスト}<NL>システム: {応答テキスト}</s>"),
-        "attention_mask": [1]*len(input_ids)
-        } ]
-    """
-    train_dataset = organize_data.pretrain_text(train_path)
-    val_dataset = organize_data.pretrain_text(val_path)
-    test_dataset = organize_data.pretrain_text(test_path)
-    print("train_dataset: ",len(train_dataset), end=", ")
-    print("max_input_ids_size: ",max([len(data["input_ids"]) for data in train_dataset]))
-    print("val_dataset: ",len(val_dataset), end=", ")
-    print("max_input_ids_size: ",max([len(data["input_ids"]) for data in val_dataset]))
-    print("test_dataset: ",len(test_dataset),end=", ")
-    print("max_input_ids_size: ",max([len(data["input_ids"]) for data in test_dataset]))
-    print("data size: ", sys.getsizeof(train_dataset))
-
-    
+def pretraining(model, tokenizer, save_path, save_model_path, save_log_path, train_dataset, val_dataset, test_dataset):
  
     # 学習中の推論結果を見るためのCallback
     prediction_callback = PredictionCallback(eval_dataset=val_dataset[:10], tokenizer=tokenizer)
@@ -159,7 +136,7 @@ def pretraining_text(save_path, save_model_path, pretrained_path, save_log_path,
             num_train_epochs=num_train_epochs,
             per_device_train_batch_size=1,
             per_device_eval_batch_size=1,
-            gradient_accumulation_steps=32,
+            gradient_accumulation_steps=64,
             learning_rate=5e-5,
             lr_scheduler_type="linear",
             warmup_ratio=0.03,
@@ -177,7 +154,7 @@ def pretraining_text(save_path, save_model_path, pretrained_path, save_log_path,
             load_best_model_at_end=True,
             label_names=["labels"],
             fp16=True,
-            dataloader_num_workers=4,
+            dataloader_num_workers=16,
         ),
         data_collator=transformers.DataCollatorForLanguageModeling(
             tokenizer, mlm=False
@@ -203,93 +180,33 @@ def pretraining_text(save_path, save_model_path, pretrained_path, save_log_path,
 
 """テキストモデルの推論
 引数
-    model_path : 推論するモデルのパラメータパス
-
+    model : 推論するモデル
+    tokenizer : 推論するトークナイザー
+    dataset : 推論するデータセット
+    ratio=0.1 : 生成する頻度 default=10%
 """
 import random
-def inference(pretrained_path, peft_name):
-    print("inference")
-    
-    tokenizer = AutoTokenizer.from_pretrained(pretrained_path, use_fast=False)
-        
-    model = AutoModelForCausalLM.from_pretrained(
-        pretrained_path,
-        device_map="sequential",
-        )
-    model = PeftModel.from_pretrained(
-        model,
-        peft_name,
-        device_map="sequence"
-    )
+def inference(model, tokenizer, dataset, ratio=0.1):
     print(model)
     model.eval()
 
     random.seed(0)
-
-    for data_type in ["debug","train", "val", "test"]:
-        print(f"build dataset for {data_type}")
-        path = f"/mnt/home/hyuga-n/E2ESpeechDialogue/S2Tdiscrete/dataset_{data_type}.json"
-        
-        data_path = open(path, "r")
-        data_path = json.load(data_path)
-        
-        for data in data_path[:10]:
-            # prompt
-            wav_text = get_wav_ids(data["input_wav_ids"])
-            input_ids = tokenize("<s>ユーザー: "+wav_text+"[SEP]"+data["input_text"], tokenizer)["input_ids"]
-            input_ids = input_ids[:-1].unsqueeze(0) # cut </s>         
-            
-            if random.random() < 1:
-                with torch.no_grad():
-                    outputs = model.generate(
-                            input_ids.to("cuda"),
-                            max_new_tokens=256,
-                            min_new_tokens=2,
-                            do_sample=True,
-                            temperature=0.8,
-                            repetition_penalty=1.2,
-                            pad_token_id=tokenizer.pad_token_id,
-                            bos_token_id=tokenizer.bos_token_id,
-                            eos_token_id=tokenizer.eos_token_id,
-                        )
-                print("answer: ",data["input_text"]+"<NL>システム: "+data["output_text"])
-                print("pred: ",tokenizer.decode(outputs[0]).split("[SEP]")[1])
-                print()
-
-
-
-parser = argparse.ArgumentParser(description="text dialogue model predict response and input's emotion label")
-
-parser.add_argument("--debug", default=True, help="True : using small debug data, False : using whole data. default=True")
-
-args = parser.parse_args()
-
-if __name__ == '__main__':
-    is_debug = args.debug # If True use debug dataset 100 size, False use train, eval and test dataset
     
-    save_path = "./Speech-to-TextDialogue/Speech-to-TextDialogueModel/ModelWeight/TextBaseModel"
-    save_model_path = "./Speech-to-TextDialogue/Speech-to-TextDialogueModel/ModelWeight/TextBaseModel"
-    pretrained_path = "rinna/japanese-gpt-neox-3.6b-instruction-ppo"
+    for data in dataset[:100]:
+        if random.random() < 0.1:
+            with torch.no_grad():
+                outputs = model.generate(
+                        data["input_ids"].unsqueeze(0).to("cuda"),
+                        max_new_tokens=256,
+                        min_new_tokens=2,
+                        do_sample=True,
+                        temperature=0.8,
+                        repetition_penalty=1.2,
+                        pad_token_id=tokenizer.pad_token_id,
+                        bos_token_id=tokenizer.bos_token_id,
+                        eos_token_id=tokenizer.eos_token_id,
+                    )
+            print("answer: ",data["input_text"]+"<NL>システム: "+data["output_text"])
+            print("pred: ",tokenizer.decode(outputs[0]).split("<NL>")[1])
+            print()
 
-    save_log_path = f"{save_path}/speech2text.log"
-    if is_debug:
-        debug_path = "/mnt/home/hyuga-n/E2ESpeechDialogue/S2Tdiscrete/dataset_debug.json"
-        train_path = val_path = test_path = debug_path
-    else:
-        train_path = "/mnt/home/hyuga-n/E2ESpeechDialogue/S2Tdiscrete/dataset_train.json"
-        val_path = "/mnt/home/hyuga-n/E2ESpeechDialogue/S2Tdiscrete/dataset_val.json"
-        test_path = "/mnt/home/hyuga-n/E2ESpeechDialogue/S2Tdiscrete/dataset_test.json"
-
-    pretraining_text(
-        save_path = save_path,
-        save_model_path=save_model_path,
-        pretrained_path=pretrained_path,
-        save_log_path = save_log_path,
-        train_path = train_path,
-        val_path = val_path,
-        test_path = test_path,
-    )
-    inference(
-        pretrained_path=save_model_path,
-        peft_name=save_model_path
-    )
